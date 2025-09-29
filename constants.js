4a809adac363aab5446ff94c0b0d903acaba5609
Abdulrehman <abdulrehman.khalik@dycoders.com>
2025-09-29 22:09:53 +0500

    data

---
+++ constants.js
@@ -0,0 +1,20 @@
+export const currency = "Rs.";
+
+export const whatsappNumber = "+923066223005";
+
+export const whatsappMessage = ` Hi! I want to order:  ${product.title} - Rs.:${product.discount_price}  
+ 
+ ${window.location.href}`;
+
+export const offerTimer = "2025-10-09T06:24:00.000Z";
+
+export const socialLinks = {
+  "facebook": "https://www.facebook.com/astelecom6",
+  "twitter": "https://twitter.com/",
+  "instagram": "https://instagram.com/",
+  "linkedin": "https://linkedin.com/company/",
+  "youtube": "https://youtube.com/",
+  "tiktok": "https://tiktok.com/",
+  "whatsapp": "https://wa.me/+923066223005",
+  "telegram": "https://t.me/"
+};
